ef6d697059ae2de6cfa67c0f357d8c369cad7ff3
austin <austin48@hawaii.edu>
2017-10-11 13:31:44 -1000

    initial commit

---
+++ jforum-impl/impl/src/java/org/etudes/component/app/jforum/JForumGBServiceImpl.java
@@ -0,0 +1,298 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/etudes/sakai-jforum/tags/2.9.11/jforum-impl/impl/src/java/org/etudes/component/app/jforum/JForumGBServiceImpl.java $ 
+ * $Id: JForumGBServiceImpl.java 83559 2013-04-30 19:03:29Z murthy@etudes.org $ 
+ *********************************************************************************** 
+ * 
+ * Copyright (c) 2008, 2009 Etudes, Inc. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ ***********************************************************************************/
+package org.etudes.component.app.jforum;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
+import org.sakaiproject.db.cover.SqlService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.etudes.api.app.jforum.JForumGBService;
+import org.sakaiproject.service.gradebook.shared.AssessmentNotFoundException;
+import org.sakaiproject.service.gradebook.shared.AssignmentHasIllegalPointsException;
+import org.sakaiproject.service.gradebook.shared.ConflictingAssignmentNameException;
+import org.sakaiproject.service.gradebook.shared.ConflictingExternalIdException;
+import org.sakaiproject.service.gradebook.shared.GradebookNotFoundException;
+import org.sakaiproject.service.gradebook.shared.GradebookService;
+import org.sakaiproject.service.gradebook.shared.GradebookExternalAssessmentService;
+
+public class JForumGBServiceImpl implements JForumGBService
+{
+	private static Log logger = LogFactory.getLog(JForumGBServiceImpl.class);
+	private Map<String,String> gradableObjectsMap = new HashMap<String,String>();
+	
+	protected GradebookExternalAssessmentService gradebookService = null;
+	
+	public void init(){
+		if (logger.isInfoEnabled()) logger.info("init....");
+	}
+	
+	public void destroy(){
+		if (logger.isInfoEnabled()) logger.info("destroy....");
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean isGradebookDefined(String context)
+	{
+		boolean hasGradebook = gradebookService.isGradebookDefined(context);
+		return Boolean.valueOf(hasGradebook);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean isExternalAssignmentDefined(String gradebookUid, String externalId)
+	{
+		boolean hasExternalAssignment = false;
+		
+		try
+		{
+			hasExternalAssignment = gradebookService.isExternalAssignmentDefined(gradebookUid, externalId);
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if (logger.isWarnEnabled()) logger.warn("isExternalAssignmentDefined:" + e.toString());
+		}
+
+		return Boolean.valueOf(hasExternalAssignment);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean isAssignmentDefined(String gradebookUid, String assignmentTitle)
+	{
+		boolean assignmentDefined = false;
+		
+		try
+		{
+			assignmentDefined = gradebookService.isAssignmentDefined(gradebookUid, assignmentTitle);
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if (logger.isWarnEnabled()) logger.warn("isAssignmentDefined:" + e.toString());
+		}
+		return Boolean.valueOf(assignmentDefined);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean addExternalAssessment(String gradebookUid, String externalId, String externalUrl, String title, double points, Date dueDate, 
+			String externalServiceDescription)
+	{
+		boolean addSuccess = false;
+		String gradableObjectKey = gradebookUid + "_" + externalId;
+		
+		try
+		{
+			gradebookService.addExternalAssessment(gradebookUid, externalId, externalUrl, title, points, dueDate, 
+					externalServiceDescription);
+			
+			addSuccess = true;
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("addExternalAssessment : "+ e.toString());
+		}
+		catch (ConflictingAssignmentNameException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("addExternalAssessment : "+ e.toString());
+		}
+		catch (ConflictingExternalIdException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("addExternalAssessment : "+ e.toString());
+		}
+		catch (AssignmentHasIllegalPointsException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("addExternalAssessment : "+ e.toString());
+		}
+		
+		// insert category back into the gb_gradable_object_t record
+        try {
+            String categoryId = gradableObjectsMap.get(gradableObjectKey);
+            if (categoryId != null) {
+                String sql = "update gb_gradable_object_t " 
+                        + "set category_id='" + categoryId + "' " 
+                        + "where external_id='" + externalId + "' "
+                        + "and gradebook_id = (select id from gb_gradebook_t where gradebook_uid='" + gradebookUid + "')";
+                
+                if (!SqlService.dbWrite(sql)) {
+                    logger.error("Unknown error writing category_id to gb_gradeable_object_t for gradebookUID: " + gradebookUid + ", externalId: " + externalId);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("ERROR writing category_id to gb_gradeable_object_t for gradebookUID: " + gradebookUid + ", externalId: " + externalId + "Exception: " + e);
+        }
+        gradableObjectsMap.remove(gradableObjectKey);
+        logger.info("gradableObjectsMap size after setting gb_gradable_object_t category_id: " + gradableObjectsMap.size());
+
+		return Boolean.valueOf(addSuccess);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean removeExternalAssessment(String gradebookUid, String externalId)
+	{
+		boolean removeSuccess = false;
+		String gradableObjectKey = gradebookUid + "_" + externalId;
+		
+		try
+		{
+            String sql = "select category_id " 
+                    + "from gb_gradebook_t as a left join gb_gradable_object_t as b on a.id = b.gradebook_id " 
+                    + "where a.gradebook_uid='" + gradebookUid + "' and b.external_id='" + externalId + "'";
+
+            List l = SqlService.dbRead(sql);
+            if (l != null && !l.isEmpty()) {
+                if (l.size() == 1) {
+                    String categoryId = null;
+                    for (Object o : l) {
+                        categoryId = (String)o;
+                        //System.out.println("Gradable Object CATEGORY_ID: " + categoryId);
+                        synchronized(gradableObjectsMap) {
+                            gradableObjectsMap.put(gradableObjectKey, categoryId);
+                            logger.info("gradableObjectsMap size after adding a category inside of synchronized block: " + gradableObjectsMap.size());
+                        }
+
+                    }
+                }
+            }
+
+			gradebookService.removeExternalAssessment(gradebookUid, externalId);
+			removeSuccess = true;
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("removeExternalAssessment : "+ e.toString());
+		}
+		catch (AssessmentNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("removeExternalAssessment : "+ e.toString());
+		}
+		
+		if (!removeSuccess) {
+			gradableObjectsMap.remove(gradableObjectKey);
+		}
+		
+		return Boolean.valueOf(removeSuccess);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean updateExternalAssessment(String gradebookUid, String externalId, String externalUrl,
+			String title, double points, Date dueDate)
+	{
+		boolean updateAssessment = false;
+		try
+		{
+			gradebookService.updateExternalAssessment(gradebookUid, externalId, externalUrl, title, points, dueDate);
+			updateAssessment = true;
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessment : "+ e.toString());
+		}
+		catch (AssessmentNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessment : "+ e.toString());
+		}
+		catch (ConflictingAssignmentNameException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessment : "+ e.toString());
+		}
+		catch (AssignmentHasIllegalPointsException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessment : "+ e.toString());
+		}
+		return Boolean.valueOf(updateAssessment);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean updateExternalAssessmentScores(String gradebookUid, String externalId, Map studentUidsToScores)
+	{
+		boolean updateScoresSuccess = false;
+		try
+		{
+			gradebookService.updateExternalAssessmentScores(gradebookUid, externalId, studentUidsToScores);
+			updateScoresSuccess = true;
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessmentScores : "+ e.toString());
+		}
+		catch (AssessmentNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessmentScores : "+ e.toString());
+		}
+		
+		return Boolean.valueOf(updateScoresSuccess);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public Boolean updateExternalAssessmentScore(String gradebookUid, String externalId, String studentUid, Double points)
+	{
+		boolean updateScoreSuccess = false;
+		try
+		{
+			gradebookService.updateExternalAssessmentScore(gradebookUid, externalId, studentUid, points.toString());
+			updateScoreSuccess = true;
+		}
+		catch (GradebookNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessmentScore : "+ e.toString());
+		}
+		catch (AssessmentNotFoundException e)
+		{
+			if(logger.isWarnEnabled()) logger.warn("updateExternalAssessmentScore : "+ e.toString());
+		}
+		
+		return Boolean.valueOf(updateScoreSuccess);
+		
+	}
+	
+	/**
+	 * @return the gradebookService
+	 */
+	public GradebookExternalAssessmentService getGradebookService()
+	{
+		return gradebookService;
+	}
+
+	/**
+	 * @param gradebookService the gradebookService to set
+	 */
+	public void setGradebookService(GradebookExternalAssessmentService gradebookService)
+	{
+		this.gradebookService = gradebookService;
+	}
+
+}
